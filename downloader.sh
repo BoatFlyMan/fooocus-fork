b8a85135a5ae66b6c692c4bfcfa09db9839d96c6
BoatFlyMan <126797451+BoatFlyMan@users.noreply.github.com>
2024-10-21 21:20:03 +0300

    Create downloader.sh

---
+++ downloader.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Loras
+download-model "https://civitai.com/api/download/models/472605?type=Model&format=SafeTensor" ./animeflatcolor.safetensors
+download-model "https://civitai.com/api/download/models/447305?type=Model&format=SafeTensor" ./overwatchstyle.safetensors
+download-model "" ./.safetensors
+# Checkpoints
