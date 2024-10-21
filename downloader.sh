fb47329a17f13e15dde1c20830415622ec54ae4e
BoatFlyMan <126797451+BoatFlyMan@users.noreply.github.com>
2024-10-21 21:35:29 +0300

    Update downloader.sh

--- downloader.sh
+++ downloader.sh
@@ -2,5 +2,11 @@
 # Loras
 download-model "https://civitai.com/api/download/models/472605?type=Model&format=SafeTensor" ./animeflatcolor.safetensors
 download-model "https://civitai.com/api/download/models/447305?type=Model&format=SafeTensor" ./overwatchstyle.safetensors
-download-model "" ./.safetensors
+download-model "https://civitai.com/api/download/models/378458?type=Model&format=SafeTensor" ./overwatchchars.safetensors
 # Checkpoints
+download-model "https://civitai.com/api/download/models/914390?type=Model&format=SafeTensor&size=full&fp=fp16" ./ponyrealism.safetensors
+download-model "https://civitai.com/api/download/models/692489?type=Model&format=SafeTensor&size=full&fp=fp16" ./ponymistoon.safetensors
+download-model "https://civitai.com/api/download/models/324619?type=Model&format=SafeTensor&size=pruned&fp=fp16" ./autismmix.safetensors
+download-model "https://civitai.com/api/download/models/324524?type=Model&format=SafeTensor&size=pruned&fp=fp16" ./autismmix_confetti.safetensors
+download-model "https://civitai.com/api/download/models/828380?type=Model&format=SafeTensor&size=pruned&fp=fp16" ./ponyprefect.safetensors
+download-model "https://civitai.com/api/download/models/832353?type=Model&format=SafeTensor&size=pruned&fp=fp16" ./realdream.safetensors
